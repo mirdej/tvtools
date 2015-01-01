12d5f6be9b9ad0c779d688efb175ee0e7d466567
mirdejavel <mirdejavel@6ce635ec-17f1-11df-8596-193289140cd3>
2015-01-01 16:42:36 +0000

    added picture snapping, session for current project, deleting of members

--- management/client/templates/members.js
+++ management/client/templates/members.js
@@ -1,8 +1,36 @@
 
   Template.memberlist.helpers({
     members: function () {
-      return Members.find({});
+        if (Session.get("show_all")){
+          return Members.find({}, {sort: {first: 1}});
+      } else {
+          return Members.find({project:Session.get("Project").id}, {sort: {first: 1}});
+      }
+    },
+    
+    member_count: function () {
+    if (Session.get("show_all")){
+      return Members.find({}).count();
+    } else {
+      return Members.find({project:Session.get("Project").id}).count();
     }
+    },
+    
+    photo: function () {
+      return Session.get("photo");
+    },
+    
+    show_all:function () {
+      return Session.get("show_all");
+    }
+  });
+  
+  Template.memberlist_row.helpers({
+    
+    show_all:function () {
+      return Session.get("show_all");
+    }
+
   });
   
   Template.memberlist.events({
@@ -15,9 +43,11 @@
     var n = event.target.tel.value;
 
     Members.insert({
+      pic:Session.get("photo"),
       first: f,
       last: l,
       tel: n,
+      project: Session.get("Project").id,
       createdAt: new Date() // current time
     });
 
@@ -25,8 +55,32 @@
     event.target.first.value = "";
     event.target.last.value = "";
     event.target.tel.value = "";
-
+    Session.set("photo","");
     // Prevent default form submit
     return false;
-  }
+  },
+  
+  'click .snap': function () {
+      var cameraOptions = {
+        width: 800,
+        height: 600
+      };
+
+      MeteorCamera.getPicture(cameraOptions, function (error, data) {
+        Session.set("photo", data);
+      });
+    },
+    
+      "click .delete": function () {
+      if (Session.get("show_all")) {
+        Members.remove(this._id);
+      } else {
+          Members.update(this._id, {$set: {project:"deleted"}});
+        }
+      },
+      
+       "change .show-all input": function (event) {
+  Session.set("show_all", event.target.checked);
+}
 });
+
--- management/global.js
+++ management/global.js
@@ -1,1 +1,22 @@
 Members = new Mongo.Collection("members");
+
+  
+if (Meteor.isClient) {
+
+
+Session.set("Project", {
+   name:"Volketswil",
+   id:"volketswil2014",
+   logo:"logo-Volketswil.png"
+});
+
+Session.set("someakwardname","H");
+
+Template.branding.helpers({
+    project_logo: function(){
+         return 'logos/'+Session.get("Project").logo;}
+});
+
+}
+
+
