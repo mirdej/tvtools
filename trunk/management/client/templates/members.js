9ba3bc3a6af600d6f45f99265bd17f045d6153de
mirdejavel <mirdejavel@6ce635ec-17f1-11df-8596-193289140cd3>
2015-01-03 10:44:36 +0000

    inline editing of memebr info

--- trunk/management/client/templates/members.js
+++ trunk/management/client/templates/members.js
@@ -22,9 +22,6 @@ Template.memberlist.helpers({
       return Session.get("show_all");
     },
     
-    doedit:function() {
- 		return Session.get("edit");
-    },
     docrop:function() {
  		return Session.get("crop");
     }
@@ -37,7 +34,10 @@ Template.memberlist_row.helpers({
     
     show_all:function () {
       return Session.get("show_all");
-    }
+    },
+    doedit:function() {
+ 		return Session.equals("edit",this._id);
+    },
 
   });
   
@@ -47,27 +47,48 @@ Template.memberlist_row.helpers({
 Template.memberlist.events({
   
     
-      "click .delete": function () {
+      "click .delete": function (e,t) {
+       e.stopPropagation();
+       e.preventDefault();
       if (Session.get("show_all")) {
         Members.remove(this._id);
       } else {
           Members.update(this._id, {$set: {project:"deleted"}});
         }
-        return false;
       },
       
-      "click .edit": function () {
-          Session.set("edit", this._id);
-          return false;
+      "click .edit": function (e,t) {
+      e.stopPropagation();
+       e.preventDefault();
+         Session.set("edit", this._id);
       },
 
-      "click .crop": function () {
+      "click .save": function (e,t) {
+       e.stopPropagation();
+       e.preventDefault();
+       
+       var id = '#'+Session.get('edit');
+       
+		var f = $(id+ ' .firstname').val()
+		var l = $(id+ ' .lastname').val()
+		var n = $(id+ ' .phone').val()
+
+		Members.update(Session.get("edit"),{$set:{
+			first: f,
+			last: l,
+			tel: n
+		}});
+		
+		Session.set("edit", null);
+      },
+
+      "click .crop": function (e,t) {
+       e.stopPropagation();
+       e.preventDefault();
           Session.set("crop", this._id);
-          return false;
       },
     
-       "change .showall input": function (event) {
-         Session.set("show_all", event.target.checked);
+       "change .showall input": function (e,t) {
+         Session.set("show_all", e.target.checked);
 }
 });
-
