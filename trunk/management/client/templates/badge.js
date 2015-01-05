082bb216d4a9a46aaf725b8f3a1afc411f6229e0
mirdejavel <mirdejavel@6ce635ec-17f1-11df-8596-193289140cd3>
2015-01-05 17:12:42 +0000

    added badge creation

---
+++ trunk/management/client/templates/badge.js
@@ -0,0 +1,17 @@
+Template.badge.helpers({
+        members: function () {
+          return Members.find({project:Session.get("Project").id}, {pic:0},{sort: {first: 1}});
+    },
+      bg: function () {
+          return "/badges/"+Session.get("Project").badge;
+      }
+})
+
+
+Template.badge.events({
+'click .ausweis' : function (e,t) {
+		e.stopPropagation();
+		e.preventDefault();
+		e.target.toggle()
+	}
+})
--- trunk/management/lib/router.js
+++ trunk/management/lib/router.js
@@ -5,4 +5,6 @@ Router.route('/mitarbeiter',		function () {this.render( 'memberlist' );});
 Router.route('/mitarbeiter/neu', 	function () {this.render( 'member_new');});
 Router.route('/mitarbeiter/crop', 	function () {this.render( 'member_crop');});
 Router.route('/mitarbeiter/lernen', function () {this.render( 'learn');});
+Router.route('/mitarbeiter/ausweis', function () {this.render( 'badge');});
 Router.route('/gruppen', 			function () {this.render( 'groups');});
+Router.route('/lueste', 			function () {this.render( 'lueste');});
