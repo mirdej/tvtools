add74cd1538aab9b281e10d86902baeaa5b6097e
mirdejavel <mirdejavel@6ce635ec-17f1-11df-8596-193289140cd3>
2015-01-06 20:19:10 +0000

    modertorenspick

--- management/client/templates/abspann.js
+++ management/client/templates/abspann.js
@@ -2,28 +2,7 @@ Template.abspann.helpers({
 
 vorlauf:function () {return Session.get('Project').abspannVorlauf; },
 all: function () {return Members.find({project:Session.get("Project").id},{sort: {first: 1}})},
-entries: function() {
 
-    var posten		= ["Sendeleitung","Moderation","Kamera","Kameraregie","Bildschnitt","Operateur","Untertitel","Ton","Licht","Fotografie","Making of","Security","Maske","Studiogast"];
-    var allMembers =  Members.find({project:Session.get("Project").id},{sort: {first: 1}}).fetch();
-
-    var result =[];
-    
-    for (var i = 0; i < posten.length; i++) {
-        var m = [];
-        var job = posten[i];
-        for (var j = 0; j < allMembers.length; j++) {
-              if ($.inArray(job, allMembers[j].posten)!=-1) {
-                m.push({'first':allMembers[j].first});
-              }
-        }
-        if (m.length) {
-          result.push({'job':job, 'members':m});
-        }    
-    }
-  return result;
-
-},
  danke: function () {
  var dank = Session.get('Project').danke;
  dank = dank.split(',');
@@ -45,3 +24,28 @@ Template.abspann.rendered = function () {
 var d = new Date();
 document.title = "Abspann "+Session.get('Project').name+"-Sendung vom "+d.toLocaleDateString();
 }
+
+Template.postenliste.helpers({
+entries: function() {
+
+    var posten		= ["Sendeleitung","Moderation","Kamera","Kameraregie","Bildschnitt","Operateur","Untertitel","Ton","Licht","Fotografie","Making of","Security","Maske","Studiogast"];
+    var allMembers =  Members.find({project:Session.get("Project").id},{sort: {first: 1}}).fetch();
+
+    var result =[];
+    
+    for (var i = 0; i < posten.length; i++) {
+        var m = [];
+        var job = posten[i];
+        for (var j = 0; j < allMembers.length; j++) {
+              if ($.inArray(job, allMembers[j].posten)!=-1) {
+                m.push({'first':allMembers[j].first});
+              }
+        }
+        if (m.length) {
+          result.push({'job':job, 'members':m});
+        }    
+    }
+  return result;
+
+}
+})
